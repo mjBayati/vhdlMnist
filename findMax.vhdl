89fd9d81361932c0c406bcd5b9b8c05fa378da1a
Mohammad kalantari <mohammadk7798@gmail.com>
2019-01-25 16:29:31 +0330

    Write findMax module.
    Module is written with loop and there may be issues with its
    synthesizability.

---
+++ findMax.vhdl
@@ -0,0 +1,28 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.array_pkg.all;
+
+entity findMax is 
+	generic (outputCnt : integer := 10);
+	port (
+		clk : in std_logic;
+		results : in inputArray(outputCnt-1 downto 0);
+		index : in integer
+	);
+end findMax;
+
+architecture implementation of findMax is 
+begin
+	process (clk) 
+		variable maxValue : std_logic_vector(15 downto 0) := (others => '0');
+		variable maxIndex : integer := 0;
+	begin
+		for i in 0 to outputCnt-1 loop
+			if signed(results(i)) > signed(maxValue) then
+				maxValue := results(i);
+				index := i;
+			end if;
+		end loop;
+		index <= i;
+	end process;
+end implementation;
